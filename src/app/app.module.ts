f1b015162b4a8192f3469082ccceb5a240831656
fethiuqam <abi_ayad.fethi_bey@courrier.uqam.ca>
2020-08-28 21:38:05 -0400

    ajout des components de base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ConnexionComponent } from './connexion/connexion.component';
+import { EditCandidatComponent } from './edit-candidat/edit-candidat.component';
+import { EditEntrepriseComponent } from './edit-entreprise/edit-entreprise.component';
+import { AccueilComponent } from './accueil/accueil.component';
+import { EditAnnonceComponent } from './edit-annonce/edit-annonce.component';
+import { AnnonceComponent } from './annonce/annonce.component';
+import { RechercheComponent } from './recherche/recherche.component';
+import { CandidatComponent } from './accueil/candidat/candidat.component';
+import { EntrepriseComponent } from './accueil/entreprise/entreprise.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ConnexionComponent,
+    EditCandidatComponent,
+    EditEntrepriseComponent,
+    AccueilComponent,
+    EditAnnonceComponent,
+    AnnonceComponent,
+    RechercheComponent,
+    CandidatComponent,
+    EntrepriseComponent
   ],
   imports: [
     BrowserModule,
