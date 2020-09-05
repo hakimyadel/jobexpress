d043dba9816b51e35d1b7077728b6320fe16403a
fethiuqam <abi_ayad.fethi_bey@courrier.uqam.ca>
2020-09-05 05:25:17 -0400

    commit de securite 7

--- src/app/annonce/annonce.component.ts
+++ src/app/annonce/annonce.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {Annonce} from "../interfaces/annonce";
+import {FirebaseAppService} from "../services/firebase-app.service";
+import {Router} from "@angular/router";
+import {experiences, niveaux} from "../interfaces/constantes";
+import {Entreprise} from "../interfaces/entreprise";
 
 @Component({
   selector: 'app-annonce',
@@ -7,7 +12,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AnnonceComponent implements OnInit {
 
-  constructor() { }
+  annonce: Annonce = {
+    nom: '',
+    creation: null,
+    diplome: null,
+    niveau: null,
+    experience: null,
+    wilaya: '',
+    telephone: '',
+    description: '',
+    image: '',
+    confirm: 'attente',
+    idEntreprise: null,
+    candidats: []
+  };
+  entreprise : Entreprise;
+  niveaux = niveaux;
+  experiences = experiences;
+
+
+  constructor(public api: FirebaseAppService, private router: Router) {
+    const that = this;
+    if (this.api.idAnnonce) {
+      this.api.app.database().ref().child('annonce').child(this.api.idAnnonce)
+        .once('value', function (snapshot) {
+          that.annonce = snapshot.val();
+        }).catch(function (error) {
+        console.log(error);
+      });
+      this.api.app.database().ref().child('entreprise').child(this.api.idUser)
+        .once('value', function (snapshot) {
+          that.entreprise = snapshot.val();
+        }).catch(function (error) {
+        console.log(error);
+      });
+
+
+    }
+  }
 
   ngOnInit(): void {
   }
